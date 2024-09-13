0c261b31815508bda4d96a07a1c919215de5b548
unknown <priyanshusingh773300@gmail.com>
2024-09-13 23:54:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+ var tl = gsap.timeline()
+
+ tl.from(".header h1",{
+    y:-300,
+    opacity:0,
+    duration:0.7,
+    delay:0.5
+
+ })
+ tl.from(".header h5",{
+     y:30,
+    opacity:0,
+    duration:0.7,
+    stagger:0.5
+
+ })
+ tl.from(".right-header .slow",{
+    y:-100,
+    opacity:0,
+    duration:0.6,
+    
+
+ })
+
+ var tl = gsap.timeline()
+ tl.from(".hero-sec button",{
+    y:10,
+    opacity:0,
+    duration:1,
+    delay:0.9
+ })
+
+
+ var tl = gsap.timeline()
+tl.from(".below-head  li",{
+    y:30,
+    opacity:0,
+    duration:1,
+    stagger:0.3
+})
+
+// var tl = gsap.timeline()
+
+// tl.from(".main .line",{
+//     x:-400,
+//     opacity:0,
+//     duration:0.9,
+//     delay:0.5,
+//     // stagger:0.3,
+//     scrollTrigger:{
+//         trigger:".main .line",
+//         scroller:"body",
+//         // markers:true,
+//         start:"top 50%",
+//         end:"top 35%",
+//         scrub:2
+
+//     }
+// })
+
+// var tl = gsap.timeline()
+
+// tl.from(".recent-post .left-img,.right-img",{
+//     x:-300,
+//     opacity:0,
+//     duration:0.6,
+//     delay:0.5,
+//     stagger:0.3,
+//     scrollTrigger:".recent-post .left-img, .right-img"
+
+// });
+ 
+
+
+
+
+
+ 
+ 
+
+ 
+ 
+ 
